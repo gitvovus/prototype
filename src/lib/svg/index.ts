7e48d068ccec2857aedb6a184a9dcbe0e89d54ed
skulinets_v <vovus.dn@gmail.com>
2019-07-29 03:38:21 +0300

    added svg parsing

--- src/lib/svg/index.ts
+++ src/lib/svg/index.ts
@@ -14,3 +14,30 @@ export class Node {
     Object.assign(this.attributes, attributes);
   }
 }
+
+export function parse(text: string) {
+  const parser = new DOMParser();
+  const document = parser.parseFromString(text, 'image/svg+xml');
+  return fromElement(document.documentElement);
+}
+
+export function fromElement(el: Element) {
+  const attributes: Attributes = {};
+  for (const attr of el.attributes) {
+    attributes[attr.name] = attr.value;
+  }
+  const node = new Node(el.tagName, attributes);
+  for (const child of el.children) {
+    node.items.push(fromElement(child));
+  }
+  return node;
+}
+
+// parse testcase:
+
+// import sample from '!!raw-loader!@/assets/fullscreen.svg';
+// function testParse() {
+//   const node = svg.parse(sample);
+//   console.log('sample:', sample);
+//   console.log('node:', node);
+// }
--- src/worker.d.ts
+++ src/worker.d.ts
@@ -1,7 +1,6 @@
-declare module "worker-loader!*" {
+declare module 'worker-loader!*' {
   class WebpackWorker extends Worker {
     constructor();
   }
-
   export default WebpackWorker;
 }
